abfabee9a28e010f1d030aaca580209311fee7c8
Kevin Johnson <kjj@aloofixschipperke.com>
2013-05-25 21:02:54 -0600

    added dejagnu 1.5.1

---
+++ specs/pkgs/dejagnu/spec.sh
@@ -0,0 +1,19 @@
+# This file is part of the Aloofix project.
+# Copyright (C) 2013 Kevin Johnson <kjj@aloofschipperke.com>
+# Distributed under the terms of the GNU General Public License version 3,
+# or (at your option) any later version.
+
+name=dejagnu
+version=1.5.1
+sequence=1
+description="A framework for testing programs"
+compression_suffix=gz
+site=http://ftp.gnu.org/gnu/$name
+depends="expect"
+builddepends=
+section=devel
+
+license=GPLv3+
+license_files=COPYING
+
+extra_doc_files="AUTHORS MAINTAINERS NEWS README TODO ChangeLog*"
